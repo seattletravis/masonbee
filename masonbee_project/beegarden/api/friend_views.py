30947b9292348073890e5664226fc7be8ec9c934
Travis Lamberte <travis@lamberte.com>
2026-03-28 15:30:11 -0700

    fixed imports

--- masonbee_project/beegarden/api/friend_views.py
+++ masonbee_project/beegarden/api/friend_views.py
@@ -1,8 +1,8 @@
+from django.db import models
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import FriendRequest, Friendship
 from .friend_serializers import (
