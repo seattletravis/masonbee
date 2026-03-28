29b94497ca4b6ee7bff3800d0c2f02ad22f5baf9
Travis Lamberte <travis@lamberte.com>
2026-03-28 15:35:47 -0700

    add urlpatterns

--- masonbee_project/beegarden/api/urls.py
+++ masonbee_project/beegarden/api/urls.py
@@ -1,6 +1,16 @@
 from rest_framework.routers import DefaultRouter
 from .views import GardenViewSet, BeeHouseViewSet, BeeHouseEventViewSet
 
+from django.urls import path
+from .friend_views import (
+    UserSearchView,
+    SendFriendRequestView,
+    PendingRequestsView,
+    AcceptFriendRequestView,
+    DeclineFriendRequestView,
+    FriendListView,
+)
+
 router = DefaultRouter()
 router.register(r"gardens", GardenViewSet, basename="garden")
 router.register(r"beehouses", BeeHouseViewSet, basename="beehouse")
@@ -8,3 +18,12 @@ router.register(r"beehouse-events", BeeHouseEventViewSet, basename="beehouse-eve
 
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("friends/search/", UserSearchView.as_view()),
+    path("friends/request/", SendFriendRequestView.as_view()),
+    path("friends/requests/", PendingRequestsView.as_view()),
+    path("friends/accept/", AcceptFriendRequestView.as_view()),
+    path("friends/decline/", DeclineFriendRequestView.as_view()),
+    path("friends/list/", FriendListView.as_view()),
+]
--- masonbee_project/config/urls.py
+++ masonbee_project/config/urls.py
@@ -25,10 +25,14 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # AIP router
     path("api/", include("beegarden.api.urls")),
+    
+    # Auth endpoints
+    path("api/register/", RegisterView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("api/me/", MeView.as_view(), name="me"),
-    path("api/register/", RegisterView.as_view(), name="register"),
 ]
