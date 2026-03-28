9de36e7cb6320ebdcad5886dbffad1b4eb8f75ff
Travis Lamberte <travis@lamberte.com>
2026-03-28 16:15:06 -0700

    all friends views and serializers completed

--- masonbee_project/beegarden/api/friend_serializers.py
+++ masonbee_project/beegarden/api/friend_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import FriendRequest, Friendship
+from beegarden.models import FriendRequest, Friendship
 
 class UserSearchSerializer(serializers.ModelSerializer):
     class Meta:
--- masonbee_project/beegarden/api/friend_views.py
+++ masonbee_project/beegarden/api/friend_views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import FriendRequest, Friendship
+from beegarden.models import FriendRequest, Friendship
 from .friend_serializers import (
     UserSearchSerializer,
     FriendRequestSerializer,
